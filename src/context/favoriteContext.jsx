4ffa668689735302cf4c55c71f73709b2b1afeef
kyawtk <kyawthantkhaing@gmail.com>
2023-07-12 14:57:47 +0630

    fixed local storage bug

--- src/context/favoriteContext.jsx
+++ src/context/favoriteContext.jsx
@@ -1,29 +1,31 @@
 import { createContext, useEffect, useState } from "react";
 
-
 export const FavoriteContext = createContext();
 
 export const FavoriteContextProvider = ({ children }) => {
-  const [favorites, setFavorites] = useState(JSON.parse(localStorage.getItem('favorites')));
- 
-  useEffect(()=>{
-    localStorage.setItem('favorites', JSON.stringify(favorites))
-  },[favorites])
+  const [favorites, setFavorites] = useState(
+    JSON.parse(localStorage.getItem("favorites")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
 
   const addFavorite = (newRecipe) => {
     setFavorites((current) => {
       return [...current, newRecipe];
     });
-    
   };
-  
-  const removeFavorite= (data)=>{
-    setFavorites(current =>{
-        return current.filter(recipe => recipe.uri !== data.uri)
-    })
-  }
+
+  const removeFavorite = (data) => {
+    setFavorites((current) => {
+      return current.filter((recipe) => recipe.uri !== data.uri);
+    });
+  };
   return (
-    <FavoriteContext.Provider value={{ favorites, addFavorite , removeFavorite}}>
+    <FavoriteContext.Provider
+      value={{ favorites, addFavorite, removeFavorite }}
+    >
       {children}
     </FavoriteContext.Provider>
   );
